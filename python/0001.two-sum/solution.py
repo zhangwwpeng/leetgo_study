cf5032ef9e7d0cba8038844b908ecf8b142a6270
wpzhang <wpzhang@wpzhangdeMacBook-Pro.local>
2026-06-21 00:15:56 +0800

    init git

---
+++ python/0001.two-sum/solution.py
@@ -0,0 +1,31 @@
+# Created by zhang-mo-peng-mo-peng-mo-mo-peng at 2026/06/19 14:23
+# leetgo: 1.4.17
+# https://leetcode.cn/problems/two-sum/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_b = 0
+        indexa, indexb = 0, 0
+        for i, num in enumerate(nums):
+            num_b = target - num
+            if num_b in nums:
+                indexa = i
+        for i, num in enumerate(nums):
+            if num_b == num:
+                indexb = i
+        return [indexa, indexb]
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    target: int = deserialize("int", read_line())
+    ans = Solution().twoSum(nums, target)
+    print("\noutput:", serialize(ans, "integer[]"))
